a54e2e277c469aefab7774f2a702fca2b4dd95e3
nisanur <nisanursahin63@gmail.com>
2026-05-01 14:39:58 +0300

    İlk commit: JavaFX proje iskeleti eklendi

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.taskmanager {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.taskmanager to javafx.fxml;
+    exports com.example.taskmanager;
+}
