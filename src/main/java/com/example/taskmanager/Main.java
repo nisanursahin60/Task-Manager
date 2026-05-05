8b0df5d269efa6356449069e6c0dba92aac221b8
nisanur <nisanursahin63@gmail.com>
2026-05-05 16:11:16 +0300

    Modülerlik tasarlandı

--- src/main/java/com/example/taskmanager/Main.java
+++ src/main/java/com/example/taskmanager/Main.java
@@ -6,13 +6,24 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
 
 public class Main extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("LoginPage.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+
+        URL fxmlUrl = Main.class.getResource("/com/example/taskmanager/LoginPage.fxml");
+
+        if (fxmlUrl == null) {
+            throw new RuntimeException("FXML bulunamadı: /com/example/taskmanager/LoginPage.fxml");
+        }
+
+        FXMLLoader fxmlLoader = new FXMLLoader(fxmlUrl);
+
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+
+        stage.setTitle("Task Manager");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,16 +1,10 @@
 module com.example.taskmanager {
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.web;
-
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires net.synedra.validatorfx;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
-    requires com.almasb.fxgl.all;
 
     opens com.example.taskmanager to javafx.fxml;
+    opens com.example.taskmanager.controller to javafx.fxml;
+
     exports com.example.taskmanager;
+    exports com.example.taskmanager.controller;
 }
