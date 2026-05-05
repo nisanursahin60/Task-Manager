288aa5153bed66f4d8a0ef2dffde48d1b1d6db35
nazaltinbas <nazaltinbas@gmail.com>
2026-05-05 17:00:44 +0300

    login page eklendi

--- src/main/java/com/example/taskmanager/controller/LoginPageController.java
+++ src/main/java/com/example/taskmanager/controller/LoginPageController.java
@@ -1,4 +1,24 @@
 package com.example.taskmanager.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class LoginPageController {
+
+    @FXML private TextField usernameField;
+    @FXML private PasswordField passwordField;
+
+    @FXML
+    private void handleLogin() {
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+
+        if (username.isEmpty() || password.isEmpty()) {
+            System.out.println("Lütfen tüm alanları doldurun!");
+        } else {
+            // Burada kullanıcı doğrulama yapıp diğer ekrana geçiş yapabilirsin
+            System.out.println("Giriş yapılıyor: " + username);
+        }
+    }
 }
